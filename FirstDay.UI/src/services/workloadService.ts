b324de205f729115cf8da36f138b18a4e5850f01
Andrew Wallace <drewsgotblues@yahoo.com>
2024-12-14 13:35:50 -0800

    Good stopping point

--- FirstDay.UI/src/services/workloadService.ts
+++ FirstDay.UI/src/services/workloadService.ts
@@ -1,10 +1,15 @@
 import api from "./api";
 import { ITEmployeeWorkload } from "../types/workload";
 
-const companyId = localStorage.getItem("companyId");
-
 export const workloadService = {
   getITEmployeeWorkload: async (): Promise<ITEmployeeWorkload[]> => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    const companyId = user.selectedCompanyId;
+
+    if (!companyId) {
+      throw new Error("Company ID not found");
+    }
+
     const { data } = await api.get(
       `/onboarding/itemployee/${companyId}/workload`
     );
